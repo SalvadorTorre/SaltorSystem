05b827bb92bfc26dfce5bdedc99d51357b7efc63
Eliu Ortega <eliu.orteg@gmail.com>
2024-06-21 08:56:00 -0400

    agregado el logo y arreglado el menu

--- src/app/features/private/private-routing.module.ts
+++ src/app/features/private/private-routing.module.ts
@@ -6,12 +6,16 @@ import { PrivatePage } from './private.page';
 const routes: Routes = [
 {
   path:"",
-  redirectTo:"mantenimientos",
+  redirectTo:"home",
   pathMatch:"full"
 },
   {path:"",
     component:PrivatePage,
     children:[
+      {
+        path:"home",
+        loadChildren: () => import('./pages/home/modulo-home').then(m => m.ModuloHome)
+      },
     {
       path:"mantenimientos",
       loadChildren: () => import('./pages/mantenimientos/modulo-mantenimiento').then(m => m.ModuloMantenimiento)
