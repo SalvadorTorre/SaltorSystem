f59047c1bad67f5685913e77f063196a465136f5
SalvadorTorre <saltor03@hotmail.com>
2024-07-24 17:35:38 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment={
   production:false,
-  backendUrl:"http://localhost:3390/api"
+ // backendUrl:"http://localhost:3390/api"
+ backendUrl:"http://148.101.59.239:3390/api"
+
 }
 
