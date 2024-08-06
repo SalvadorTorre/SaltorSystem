8659163f56e609de5e2fe61481b6b8cf03ca5f39
SalvadorTorre <saltor03@hotmail.com>
2024-08-06 17:12:26 -0400

    cotizacion

---
+++ src/app/features/private/pages/cotizacion/cotizacion-modulo.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Cotizacion} from './cotizacion';
+import { RutaCotizacion } from './cotizacion-ruta';
+
+@NgModule({
+  declarations: [
+    Cotizacion
+  ],
+  imports: [
+    CommonModule,
+    RutaCotizacion
+  ],
+
+  providers: [],
+  bootstrap: [Cotizacion]
+})
+export class ModuloCotizacion { }
---
+++ src/app/features/private/pages/cotizacion/cotizacion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cotizacion',
+  templateUrl: './cotizacion.html',
+  styleUrls: ['./cotizacion.css']
+})
+export class Cotizacion {
+}
