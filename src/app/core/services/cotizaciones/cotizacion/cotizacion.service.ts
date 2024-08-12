98f4794f0a02113da8a4e751163ed276f1881021
SalvadorTorre <saltor03@hotmail.com>
2024-08-11 23:52:59 -0400

    cotizAXION

---
+++ src/app/core/services/cotizaciones/cotizacion/cotizacion.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpInvokeService } from "../../http-invoke.service";
+import { CotizacionModel, CotizacionModelData } from ".";
+
+@Injectable({
+    providedIn: "root"
+})
+export class ServicioCotizacion {
+    constructor(private http: HttpInvokeService) { }
+
+    guardarCotizacion(cotizacion: any): Observable<any> {
+        return this.http.PostRequest<any, any>("/cotizacion", cotizacion);
+    }
+
+    editarCotizacion(ct_codcoti: string, cotizacion: CotizacionModel): Observable<any> {
+        return this.http.PutRequest<any, any>(`/cotizacion/${ct_codcoti}`, cotizacion);
+    }
+
+    buscarTodasCotizacion(pageIndex: number, pageSize: number, descripcion?: string): Observable<any> {
+        let url = `/cotizacion?page=${pageIndex}&limit=${pageSize}`;
+        if (descripcion) {
+            url += `&descripcion=${descripcion}`;
+        }
+        console.log(url);
+        return this.http.GetRequest<any>(url);
+    }
+
+    eliminarCotizacion(ct_codcoti: string): Observable<any> {
+        return this.http.DeleteRequest(`/cotizacion/$ct_codcoti}`, "");
+    }
+
+    buscarCotizacion(ct_codcoti: string): Observable<any> {
+        return this.http.GetRequest<any>(`/cotizacion/${ct_codcoti}`);
+    }
+
+
+
+}
--- src/app/features/private/pages/cotizacion/cotizacion.ts
+++ src/app/features/private/pages/cotizacion/cotizacion.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ɵNG_COMP_DEF } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import Swal from 'sweetalert2';
+import { ServicioCotizacion } from 'src/app/core/services/cotizaciones/cotizacion/cotizacion.service';
+import { CotizacionModelData, detCotizacionData } from 'src/app/core/services/cotizaciones/cotizacion';
+declare var $: any;
 @Component({
   selector: 'cotizacion',
   templateUrl: './cotizacion.html',
