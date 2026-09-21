c1de1fbc9f7aaa613785e3ba96b988c9f8fcfd8f
SalvadorTorre <saltor03@hotmail.com>
2026-09-21 10:01:06 -0300

    build(environment): swap dev supabase url and fix indentation
    update active Supabase API endpoint for development environment, fix inconsistent indentation of the anonKey credential in both environment configuration files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  appVersion: '1.0.161',
+  appVersion: '1.0.162',
   bypassAuth: false,
   // backendUrl: 'http://localhost:3390/api',
   backendUrl: 'https://grupohierro.duckdns.org/api',
